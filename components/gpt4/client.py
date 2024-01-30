1ce77cb054f8ead127782d71f1b1cd93109176ef
karanpraharaj <karanpraharaj@hotmail.com>
2024-01-30 12:41:13 -0600

    refactor: moving dotenv load away to client caller

--- components/gpt4/client.py
+++ components/gpt4/client.py
@@ -1,13 +1,8 @@
-from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-
-env_file_path = os.path.join(ROOT_DIR, "proj.env")
-
-def instantiate_client():
-    load_dotenv(dotenv_path=env_file_path)
-    client = OpenAI()
+def instantiate_client(api_key):
+    
+    client = OpenAI(api_key=api_key)
 
     return client

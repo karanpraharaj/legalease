f19fce350025a6602e5232ef6d9715e63c371874
karanpraharaj <karanpraharaj@hotmail.com>
2024-01-31 14:00:09 -0600

    revert: a mess up

--- server/endpoints/transcription.py
+++ server/endpoints/transcription.py
@@ -1,14 +1,10 @@
-import logging
-import uuid
-
-from flask import request
 from flask_restx import Resource
 
 from server.frameworks.api import api
 from server.frameworks.serializers import transcription_text, upload_parser
 
 from src.upload_audio import store_audio_file, allowed_file
-from src.transcribe import run_transcribe, get_models_path, get_audios_path
+from src.transcribe import run_transcribe
 
 ns = api.namespace('transcription', description='Operations related to audio transcription')
 
@@ -37,11 +33,7 @@ class TranscriptionSubmit(Resource):
         stored_filepath = store_audio_file(uploaded_file)
 
         transcription = run_transcribe(
-            models_path="./components/whisper.cpp/models",
-            audios_path="./components/whisper.cpp/samples",
-            audio_filename=stored_filepath,
-            model_name="ggml-base.bin",
-            image_name="test-4"
+            audio_filepath=stored_filepath,
         )
 
         return_json = {

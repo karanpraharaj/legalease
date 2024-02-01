135b7a7f7e64a9cc797eb4f01d7be23158e95287
karanpraharaj <karanpraharaj@hotmail.com>
2024-02-01 13:07:50 -0600

    feat: adding input parser for classify

--- server/frameworks/serializers.py
+++ server/frameworks/serializers.py
@@ -17,6 +17,24 @@ transcription_text = api.model('Transcription Text', {
                           example='This is a transcription.'),
 })
 
+summarization_text = api.model('Summarization Text', {
+    'text': fields.String(required=True,
+                          description='Summarization text.',
+                          example='This is a summary.'),
+})
+
+classification_text = api.model('Classification Text', {
+    'decision': fields.String(required=True,
+                                description='Classification decision.',
+                                example='This is a classification.'),
+    'reason': fields.String(required=True,
+                            description='Classification reason.',
+                            example='This is a reason.'),
+    'citations': fields.String(required=True,
+                            description='Citations for classification.',
+                            example='This is a citation.'),
+})
+
 # https://flask-restx.readthedocs.io/en/latest/parsing.html#file-upload
 upload_parser = api.parser()
 upload_parser.add_argument('file', location='files', type=FileStorage, required=True, help='Audio file to be transcribed.')
@@ -24,3 +42,7 @@ upload_parser.add_argument('file', location='files', type=FileStorage, required=
 summarization_input_parser = api.parser()
 summarization_input_parser.add_argument('review_findings', type=str, required=True, help='Text to be summarized.')
 summarization_input_parser.add_argument('instructions', type=str, required=False, help='Instructions for summarization.')
+
+classification_input_parser = api.parser()
+classification_input_parser.add_argument('doc_text', type=str, required=True, help='Text to be classified.')
+classification_input_parser.add_argument('issue', type=str, required=True, help='Issue to be classified.')
